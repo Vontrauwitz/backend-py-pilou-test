50a06322af2f3c43a0114a22cc167c8c08972743
Hans Trauwitz <hans.trauwitz@gmail.com>
2023-07-26 07:56:50 -0600

    agregando query parameter apra filtrar

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-  fullName= models.CharField(max_length=30)
-  birthDate = models.DateField()
-  telephone = models.CharField(max_length=15)
-  email = models.EmailField(max_length=50)
-  image = models.CharField(max_length=1500)
-  userName = models.CharField(max_length=30, unique=True)
+  fullName= models.CharField(max_length=30,blank=True, null=True)
+  age = models.CharField(max_length=30,blank=True, null=True)
+  telephone = models.CharField(max_length=15,blank=True, null=True)
+  email = models.EmailField(max_length=50,blank=True, null=True)
+  image = models.CharField(max_length=1500, blank=True, null=True)
+  userName = models.CharField(max_length=30, blank=True, null=True)
   
--- api/views.py
+++ api/views.py
@@ -1,10 +1,35 @@
-# from django.shortcuts import render
+# # from django.shortcuts import render
+# from rest_framework import viewsets
+# from .serializer import UserSerializer
+# from .models import User
+
+# # Create your views here.
+
+# class UserViewSet(viewsets.ModelViewSet):
+#   queryset = User.objects.all()[:2]
+#   serializer_class = UserSerializer
+
+
+from django.shortcuts import render
 from rest_framework import viewsets
 from .serializer import UserSerializer
 from .models import User
 
-# Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.request.query_params.get('age'):
+            queryset = queryset.filter(age=self.request.query_params.get('age'))
+
+        if self.request.query_params.get('email'):
+            queryset = queryset.filter(email=self.request.query_params.get('email'))
+
+        if self.request.query_params.get('phone_number'):
+            queryset = queryset.filter(phone_number=self.request.query_params.get('phone_number'))
+
+        return queryset
